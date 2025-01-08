a32cf7a449636ea8d9742986d7123d4bd836816b
Ademe <A.AYAD@lprs.fr>
2025-01-08 15:20:57 +0100

    [feat] Explication des règles
    règles + présentation du jeu

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Bienvenue dans le jeu du MasterMind !");
+        System.out.println("Le MasterMind est un jeu de logique classique où le joueur tente de deviner une séquence \n" +
+                "secrète de chiffres. Dans ce projet, l'objec f est de créer une version numérique du jeu en \n" +
+                "u lisant le langage de programma on Java.");
+        System.out.println("Les règles sont simples : ");
+        System.out.println("1- La séquence secrète est composée de chiffres de 1 à 9, sans répétition.");
+        System.out.println("2- Le joueur soumet des propositions de séquences, et les indices sont donnés en fonction des chiffres corrects et bien placés, ainsi que des chiffres corrects mais mal placés.");
+        System.out.println("3- Le joueur a un nombre limité de tentatives pour deviner la séquence secrète. ");
+        System.out.println("4- La partie se termine lorsque le joueur trouve la séquence secrète ou épuise toutes ses tentatives. ");
     }
 }
