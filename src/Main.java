1ba79cb9f99f114b6cce51aa4f2655d664de1d03
Ademe <A.AYAD@lprs.fr>
2025-01-08 22:15:55 +0100

    [feat] Choisir de jouer
    Le joueur, après l'explication des règles, peut décider de lancer la
    partie ou de ne pas jouer.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+g
 public class Main {
     public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
         System.out.println("Bienvenue dans le jeu du MasterMind !");
         System.out.println("Le MasterMind est un jeu de logique classique où le joueur tente de deviner une séquence \n" +
-                "secrète de chiffres. Dans ce projet, l'objec f est de créer une version numérique du jeu en \n" +
-                "u lisant le langage de programma on Java.");
+                "secrète de chiffres. Dans ce projet, l'objectif est de créer une version numérique du jeu en \n" +
+                "utilisant le langage de programmation Java.");
         System.out.println("Les règles sont simples : ");
         System.out.println("1- La séquence secrète est composée de chiffres de 1 à 9, sans répétition.");
         System.out.println("2- Le joueur soumet des propositions de séquences, et les indices sont donnés en fonction des chiffres corrects et bien placés, ainsi que des chiffres corrects mais mal placés.");
         System.out.println("3- Le joueur a un nombre limité de tentatives pour deviner la séquence secrète. ");
         System.out.println("4- La partie se termine lorsque le joueur trouve la séquence secrète ou épuise toutes ses tentatives. ");
+        System.out.println("Voulez vous commencer la partie ? (o/n)");
+        String commencerPartie = clavier.nextLine().toUpperCase();
+        if (commencerPartie.equals("o")) {
+
+        }else {
+            System.out.println("Bon bah au revoir !");
+        }
     }
 }
